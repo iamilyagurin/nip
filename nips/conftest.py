9c3d47d9b6411088020368ff7591d04aff5e6239
Ilya Gurin <iamilyagurin@gmail.com>
2022-05-03 00:07:24 +0300

    cookie cutter django

---
+++ nips/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from nips.users.models import User
+from nips.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
